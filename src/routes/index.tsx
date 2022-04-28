b14335ab376443a18c3c8627de21906351c9c325
vietanh <vietanh@vietanhs-MacBook-Pro.local>
2022-04-28 17:56:47 +0700

    update: routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, memo, Suspense, lazy } from "react";
-import { Routes, Route } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 
 import { Loading } from "components";
 import { ROUTES } from "common/constant";
@@ -11,58 +11,50 @@ const Register = lazy(() => import("../components/Register"));
 const ForgotPassword = lazy(() => import("../components/ForgotPassword"));
 const Chat = lazy(() => import("../components/ChannelContainer"));
 
+const NotFoundPage: FC = () => <p>There is nothing here: 404!</p>;
+
 const AppRoutes: FC = () => {
   const user = localStorage.getItem("user");
   const isLogged = user ? JSON.parse(user).user : false;
 
-  return (
-    <Suspense fallback={<Loading />}>
-      <Routes>
-        <Route
-          path={ROUTES.HOME}
-          element={
-            <ProtectedRoutes condition={isLogged} redirectTo={ROUTES.LOGIN}>
-              <ProtectedRoutes condition={!isLogged} redirectTo={ROUTES.CHAT}>
-                <Login />
-              </ProtectedRoutes>
-            </ProtectedRoutes>
-          }
-        />
+  const element = useRoutes([
+    {
+      path: "/",
+      element: <NotFoundPage />,
+    },
+    {
+      path: ROUTES.HOME,
+      element: (
+        <ProtectedRoutes condition={!isLogged} redirectTo={ROUTES.LOGIN}>
+          <CommonLayout />
+        </ProtectedRoutes>
+      ),
+      children: [
+        {
+          path: ROUTES.LOGIN,
+          element: <Login />,
+        },
+        {
+          path: ROUTES.REGISTER,
+          element: <Register />,
+        },
+        {
+          path: ROUTES.FORGOT_PASSWORD,
+          element: <ForgotPassword />,
+        },
+      ],
+    },
+    {
+      path: ROUTES.CHAT,
+      element: (
+        <ProtectedRoutes condition={isLogged} redirectTo={ROUTES.CHAT}>
+          <Chat />
+        </ProtectedRoutes>
+      ),
+    },
+  ]);
 
-        <Route
-          path={ROUTES.CHAT}
-          element={
-            <ProtectedRoutes condition={isLogged} redirectTo={ROUTES.LOGIN}>
-              <Chat />
-            </ProtectedRoutes>
-          }
-        />
-
-        <Route
-          path={ROUTES.LOGIN}
-          element={
-            <ProtectedRoutes condition={!isLogged} redirectTo={ROUTES.CHAT}>
-              <Chat />
-            </ProtectedRoutes>
-          }
-        />
-
-        <Route path={ROUTES.LOGIN} element={<CommonLayout />}>
-          <Route path={ROUTES.LOGIN} element={<Login />} />
-        </Route>
-
-        <Route path={ROUTES.REGISTER} element={<CommonLayout />}>
-          <Route path={ROUTES.REGISTER} element={<Register />} />
-        </Route>
-
-        <Route path={ROUTES.FORGOT_PASSWORD} element={<CommonLayout />}>
-          <Route path={ROUTES.FORGOT_PASSWORD} element={<ForgotPassword />} />
-        </Route>
-
-        <Route path="*" element={<p>There&apos;s nothing here: 404!</p>} />
-      </Routes>
-    </Suspense>
-  );
+  return <Suspense fallback={<Loading />}>{element}</Suspense>;
 };
 
 export default memo(AppRoutes);
